89605e1b04c21fb723cab97bcc5b56c99b50dca3
Ricardo Alcantara da Silva <brasilricks@hotmail.com>
2023-11-09 00:59:55 -0300

    Inicio do Projeto Teste

---
+++ Teste01/Teste01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Teste01.Startup))]
+namespace Teste01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
